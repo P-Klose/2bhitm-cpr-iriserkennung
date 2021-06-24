2519503c6462cfa911a68f678cbb42cfc0fba6ae
P-Klose <71823734+P-Klose@users.noreply.github.com>
2021-06-24 11:24:51 +0200

    Add files via upload

--- Server/server.js
+++ Server/server.js
@@ -1,25 +1,39 @@
+let allTimeFaces = 0;
+let allTimeEyes = 0;
+let FacesNow;
+let EyesNow;
+
 
 const express = require("express");
 const app = express();
 const port = 3000;
 
-let myData = [];
 
 app.listen(port, ()=> {
     console.log("server läuft unter http://localhost:"+port);
 })
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // DATEN VOM CLIENT EMPFANGEN
 app.post('/chartData', (req, res) => {
     console.log('HALLO DER REQUEST IST DA!');
-    let data = req.body;
-    myData.push(data)
+    FacesNow = req.faces;
+    EyesNow = req.eyes;
+
+    if(allTimeEyes<EyesNow){
+        allTimeEyes = EyesNow;
+    }
+    if(allTimeFaces<FacesNow){
+        allTimeFaces = FacesNow;
+    }
+    //myData.push(data)
 
 })
 
 app.get('/chartData', (req, res) => {
+    let myData = {'facesNow':FacesNow,'eyesNow':EyesNow,'facesHigh':allTimeFaces,'eyesHigh':allTimeEyes};
     res.send( JSON.stringify(myData) );
 })
 
